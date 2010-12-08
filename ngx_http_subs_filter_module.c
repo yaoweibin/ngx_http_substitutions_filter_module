25b03f2869ed997fc6c9c395009a4223d4b7a9bc
Weibin Yao <yaoweibin@gmail.com>
2010-12-08 05:14:32 +0000

    fix the function  memmem() is not included in the C library
    git-svn-id: http://substitutions4nginx.googlecode.com/svn/trunk@42 184bbb60-1f5e-11de-b650-e715bd6d7cf1

--- ngx_http_subs_filter_module.c
+++ ngx_http_subs_filter_module.c
@@ -860,6 +860,45 @@ ngx_http_subs_match_regex_substituion(ngx_http_request_t *r,
 }
 
 
+#if defined(__OpenBSD__)
+
+/*
+ * Thanks to Laurent Ghigonis
+ * Taken from FreeBSD
+ * Find the first occurrence of the byte string s in byte string l.
+ */
+static void *
+memmem(const void *l, size_t l_len, const void *s, size_t s_len)
+{
+    register char *cur, *last;
+    const char *cl = (const char *)l;
+    const char *cs = (const char *)s;
+
+    /* we need something to compare */
+    if (l_len == 0 || s_len == 0)
+        return NULL;
+
+    /* "s" must be smaller or equal to "l" */
+    if (l_len < s_len)
+        return NULL;
+
+    /* special case where s_len == 1 */
+    if (s_len == 1)
+        return memchr(l, (int)*cs, l_len);
+
+    /* the last position where its possible to find "s" in "l" */
+    last = (char *)cl + l_len - s_len;
+
+    for (cur = (char *)cl; cur <= last; cur++)
+        if (cur[0] == cs[0] && memcmp(cur, cs, s_len) == 0)
+            return cur;
+
+    return NULL;
+}
+
+#endif
+        
+
 static ngx_int_t
 ngx_http_subs_match_fix_substituion(sub_pair_t *pair, 
         ngx_buf_t *b, ngx_buf_t *dst, ngx_pool_t *tpool)

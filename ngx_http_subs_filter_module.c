e420fb31a73b65399a2336c091a2a9bb47769c67
Weibin Yao <yaoweibin@gmail.com>
2012-04-10 04:01:50 +0000

    fix an compatible problem with gzip filter
    git-svn-id: http://substitutions4nginx.googlecode.com/svn/trunk@58 184bbb60-1f5e-11de-b650-e715bd6d7cf1

--- ngx_http_subs_filter_module.c
+++ ngx_http_subs_filter_module.c
@@ -1059,7 +1059,7 @@ ngx_http_subs_output( ngx_http_request_t *r,
         }
 
         if (cl->next == NULL) {
-            b->flush = 1;
+            b->sync = 1;
             b->last_buf = last_chain;
         }
     }

48dd4b02963bbd21a99fa95381a7b3e4acd25b7f
Weibin Yao <yaoweibin@gmail.com>
2012-04-19 22:17:44 +0800

    move the docs

--- util/update-readme.sh
+++ util/update-readme.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-perl util/wiki2pod.pl README.wiki > /tmp/a.pod && pod2text /tmp/a.pod > README
+perl util/wiki2pod.pl doc/README.wiki > /tmp/a.pod && pod2text /tmp/a.pod > README
 
-perl util/wiki2pod.pl README.wiki > /tmp/a.pod && pod2html /tmp/a.pod > README.html
+perl util/wiki2pod.pl doc/README.wiki > /tmp/a.pod && pod2html /tmp/a.pod > doc/README.html
 
-perl util/wiki2google_code_homepage.pl README.wiki > README.google_code_home_page.wiki
+perl util/wiki2google_code_homepage.pl doc/README.wiki > doc/README.google_code_home_page.wiki

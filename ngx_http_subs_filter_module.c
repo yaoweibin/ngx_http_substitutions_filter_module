6259c4e5a77ea2c523b2cfcf0aa2594fffca10cd
Weibin Yao <yaoweibin@gmail.com>
2012-07-05 23:28:27 +0800

    fix uncompatible code with nginx-1.2.2

--- ngx_http_subs_filter_module.c
+++ ngx_http_subs_filter_module.c
@@ -1161,7 +1161,7 @@ ngx_http_subs_regex_capture_count(ngx_regex_t *re)
 
     n = 0;
 
-#if defined(nginx_version) && nginx_version >= 1003001
+#if defined(nginx_version) && nginx_version >= 1002002
     rc = pcre_fullinfo(re->code, NULL, PCRE_INFO_CAPTURECOUNT, &n);
 #elif defined(nginx_version) && nginx_version >= 1001012
     rc = pcre_fullinfo(re->pcre, NULL, PCRE_INFO_CAPTURECOUNT, &n);

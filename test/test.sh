f87b5dd21e09149941f0bd646ff687ccf1a2d27c
cube <hourui.me@gmail.com>
2015-01-05 17:30:46 +0800

    Fullfill the tests and docs. 1. add test/t/subs_none_regex.t 2. modify README doc/README 3. making test/test.sh more friendly

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,40 @@
 #!/bin/sh
 
-#TEST_NGINX_BACKENDS_PORT=127.0.0.1:1234 PATH=/home/yaoweibin/nginx/sbin:$PATH prove -r t
+# 
+# Test::LongString Module of perl is needed
+# You can install it with `cpan install Test::LongString`
+#
 
-PATH=/home/yaoweibin/nginx/sbin:$PATH prove -r t
+#
+# Basic Usage.
+#  TEST_NGINX_SBIN_PATH=/path/to/nginx/sbin \
+#  ./test.sh
+#
+
+#
+# Custom Backend Server
+#  TEST_NGINX_SBIN_PATH=/path/to/nginx/sbin \
+#  TEST_NGINX_BACKENDS_PORT=ip:port \
+#  ./test.sh
+
+TEST_NGINX_SBIN_PATH=${TEST_NGINX_SBIN_PATH:=/path/to/nginx/sbin}
+PATH=$TEST_NGINX_SBIN_PATH:$PATH prove -r t
+
+#
+# The command of @yaoweibin
+#
+# nginx sbin path
+# TEST_NGINX_SBIN_PATH=/home/yaoweibin/nginx/sbin
+
+# 
+# basic test with backend www.taobao.com:80
+#
+# PATH=$TEST_NGINX_SBIN_PATH:$PATH prove -r t
+
+#
+# custom test with custom backend
+#
+# TEST_NGINX_BACKENDS_PORT=127.0.0.1:1234 \
+# PATH=$TEST_NGINX_SBIN_PATH:$PATH prove -r t
+
+
